f5229171aa8a60b508915cf7c2e6a2416f6e7bc3
eturino <eturino@eturino.com>
2024-11-06 13:48:52 +0100

    feat: remove lodash
    - also adapt to new project structure

--- src/initials.ts
+++ src/initials.ts
@@ -1,8 +1,6 @@
-import compact from "lodash.compact";
-
 export function initials(label: string): string {
   const splitted: string[] = label.split(" ");
-  const parts = compact(splitted);
+  const parts = splitted.filter(Boolean);
   const initials = parts.map((part) => part.charAt(0));
   return initials.join(" ");
 }
